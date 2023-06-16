bf8f586a9cc95db5dacec863f2766c638ad99ea2
karthicksadha <karthicksadha24@gmail.com>
2023-06-16 18:06:07 +0000

    adminside star-rating done

---
+++ angularapp/src/app/components/adminside/star-rating/star-rating.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-star-rating',
+  templateUrl: './star-rating.component.html',
+  styleUrls: ['./star-rating.component.css']
+})
+export class StarRatingComponent {
+  @Input() rating: number;
+
+  stars: number[] = [1, 2, 3, 4, 5];
+}
