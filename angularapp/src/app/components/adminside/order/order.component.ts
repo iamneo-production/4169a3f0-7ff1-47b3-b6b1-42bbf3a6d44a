45e24b2c7f5a40789a2c6fe3214718ac51f2e5ae
adhikesav <kesavmurali1611@gmail.com>
2023-06-20 09:59:43 +0000

    Order_Admin_Done

--- angularapp/src/app/components/adminside/order/order.component.ts
+++ angularapp/src/app/components/adminside/order/order.component.ts
@@ -6,6 +6,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent implements OnInit {
+  gifts=[
+    {
+      "orderId" : "1",
+      "userId"  :"User1",
+      "giftName":"Wooden frame",
+      "giftPrice": "200",
+      "giftQuantity": "3pcs",
+      },
+    {
+        "orderId" : "2",
+        "userId"  :"User2",
+        "giftName":"Wooden frame",
+        "giftPrice": "200",
+        "giftQuantity": "5pcs",
+        },
+        {
+          "orderId" : "3",
+          "userId"  :"User3",
+          "giftName":"Wooden frame",
+          "giftPrice": "200",
+          "giftQuantity": "7pcs",
+          },
+          {
+            "orderId" : "4",
+            "userId"  :"User4",
+            "giftName":"Wooden frame",
+            "giftPrice": "200",
+            "giftQuantity": "10pcs",
+            },
+            {
+              "orderId" : "4",
+              "userId"  :"User4",
+              "giftName":"Wooden frame",
+              "giftPrice": "200",
+              "giftQuantity": "10pcs",
+              },
+              {
+                "orderId" : "4",
+                "userId"  :"User4",
+                "giftName":"Wooden frame",
+                "giftPrice": "200",
+                "giftQuantity": "10pcs",
+                },
+                {
+                  "orderId" : "4",
+                  "userId"  :"User4",
+                  "giftName":"Wooden frame",
+                  "giftPrice": "200",
+                  "giftQuantity": "10pcs",
+                  },
+                  {
+                    "orderId" : "4",
+                    "userId"  :"User4",
+                    "giftName":"Wooden frame",
+                    "giftPrice": "200",
+                    "giftQuantity": "10pcs",
+                    },
+                    {
+                      "orderId" : "4",
+                      "userId"  :"User4",
+                      "giftName":"Wooden frame",
+                      "giftPrice": "200",
+                      "giftQuantity": "10pcs",
+                      },
+  ]
 
   constructor() { }
 
