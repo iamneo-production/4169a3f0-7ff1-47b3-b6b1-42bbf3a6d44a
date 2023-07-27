603bb0a30867dfb413714e2dfb505b780bf8d48b
prab-3 <tejeshsuresh6@gmail.com>
2023-07-27 07:40:23 +0000

    manual routing done

--- angularapp/src/app/user.service.ts
+++ angularapp/src/app/user.service.ts
@@ -8,7 +8,9 @@ import { User } from './user';
 })
 export class UserService {
 
-  private apiUrl = 'https://8080-afebfaaebebecfdadebbecffccbcffabaefd.project.examly.io/admin/users';
+  private apiUrl = 'https://8080-bdfcbfadbcfdadebbecffccbcffabaefd.project.examly.io/admin/users';
+  private isLoggedInFlag: boolean = false;
+  private userRole: string = '';
 
 
 
@@ -19,6 +21,30 @@ export class UserService {
   }
 
   addUser(user: User): Observable<User> {
-    return this.http.post<User>(this.apiUrl, user);
-  }
+    return this.http.post<User>(this.apiUrl,user);
+  }
+  // Method to set user role (you can call this method after successful login)
+  setUserRole(role: string): void {
+    this.userRole = role;
+  }
+
+  // Method to get user role
+  getUserRole(): string {
+    return this.userRole;
+  }
+
+  // Method to set login status (you can call this method after successful login/logout)
+  setLoggedInStatus(status: boolean): void {
+    this.isLoggedInFlag = status;
+  }
+
+  // Method to check if the user is logged in
+  isLoggedIn(): boolean {
+    return this.isLoggedInFlag;
+  }
+  logout(): void {
+    this.isLoggedInFlag = false;
+    this.userRole = '';
+  }
+
 }
